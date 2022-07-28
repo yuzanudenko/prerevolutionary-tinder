0f346458c02d313d4272958f50ff7c530a7004ef
Guzime <40675616+Guzime@users.noreply.github.com>
2022-07-28 21:11:33 +0300

    test

--- prerevolutionary-tinder-tgbot-client/src/main/java/ru/liga/tgbot/Application.java
+++ prerevolutionary-tinder-tgbot-client/src/main/java/ru/liga/tgbot/Application.java
@@ -3,6 +3,8 @@ package ru.liga.tgbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Test
+
 @SpringBootApplication
 public class Application {
 
